c162f4b8c3b0211508780d8fbb3dc4dd30ce8c48
Michael Prattinger <mprattinger@outlook.com>
2020-01-14 13:09:41 +0100

    Add project files.

---
+++ FlintSoftFood.Contracts/Models/Food.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FlintSoftFood.Contracts.Models
+{
+    public class Food
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public int EAN { get; set; }
+
+        [Required]
+        public int Calories { get; set; }
+
+        [Required]
+        public decimal Carbohydrates { get; set; }
+
+        public decimal Fat { get; set; }
+    }
+}
---
+++ FlintSoftFood.DAL/Infrastructure/FoodContext.cs
@@ -0,0 +1,22 @@
+﻿using FlintSoftFood.Contracts.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FlintSoftFood.DAL.Infrastructure
+{
+    public class FoodContext : DbContext
+    {
+        public DbSet<Food> Food { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName("Food_" + entity.GetTableName());
+
+            }
+        }
+    }
+}
